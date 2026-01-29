7b294ef4a68d83d90f97989d94899de5de684933
studywithusmans <studywithusmans@gmail.com>
2026-01-29 07:19:37 +0000

    fix: Add Firebase server initialization and fix build

---
+++ src/firebase/server.ts
@@ -0,0 +1,11 @@
+import { initializeApp, getApps, cert } from "firebase-admin/app";
+
+const serviceAccount = JSON.parse(import.meta.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
+
+const apps = getApps();
+
+export const app = apps.length
+  ? apps[0]
+  : initializeApp({
+      credential: cert(serviceAccount),
+    });
